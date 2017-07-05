166717ebc10880332e3abc7f104c88e251a71313
Pablo S <psgonza@users.noreply.github.com>
2017-07-05 10:48:40 +0200

    Update tiny_tcp_port_scanner.py

--- tiny_tcp_port_scanner.py
+++ tiny_tcp_port_scanner.py
@@ -17,32 +17,42 @@
   * Finished scan on 192.168.1.1 ports 1-1024
 
 """
-
+#!/usr/bin/env python
 from socket import *
-import sys
+import signal
+from sys import exit
 
+s_timeout=0.2
 target = raw_input('1) Enter host or ip to scan: ')
+
+def signal_handler(signal, frame):
+    print('\n* Ctrl+C captured... Exiting')
+    exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+    
 try:
     initial = int(raw_input('2) Enter first port: '))
     final = int(raw_input('3) Enter last port: '))
 except ValueError:
-    print("\n-E- Please enter an integer as first/last port")
-    sys.exit()
+    print("\n-E- Please enter an integer as first/last port\n")
+    exit()
 
 try:
     targetIP = gethostbyname(target)
 except:
-    print('\n-E- {} is not a valid IP address'.format(target))
-    sys.exit()
+    print('\n-E- {0} is not a valid IP address\n'.format(target))
+    exit()
 
-print('\n* Starting scan on {} ports {}-{}'.format(targetIP,initial,final))
+print('\n* Starting scan on {0} ports {1}-{2}\n'.format(targetIP,initial,final))
 
 for i in range(initial,final):
     s = socket(AF_INET, SOCK_STREAM)
-    s.settimeout(0.2)
+    s.settimeout(s_timeout)
     result = s.connect_ex((targetIP, i))
     if(result == 0) :
-        print('(TCP) Port {}: OPEN'.format(i))
+        print('(TCP) Port {0}: OPEN'.format(i))
     s.close()
 
-print('\n* Finished scan on {} ports {}-{}'.format(targetIP,initial,final))
+print('\n* Finished scan on {0} ports {1}-{2}\n'.format(targetIP,initial,final))
+
